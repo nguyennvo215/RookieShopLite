cf9489392b5db286eed93648a9fc240c2f14142d
DESKTOP-D23Q2I8\shigo <vanguyen215@gmail.com>
2021-03-31 15:33:20 +0700

    Implement BrandController.cs

--- RookieShopLite/Apis/BrandController.cs
+++ RookieShopLite/Apis/BrandController.cs
@@ -1,11 +1,101 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RookieShopLite.Data;
+using RookieShopLite.Model;
+using RookieShopLite.ViewModel;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace RookieShopLite.Apis
 {
-    public class BrandController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
+
+        public BrandController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<ActionResult<IEnumerable<BrandViewModel>>> GetBrands()
+        {
+            return await _context.Brands
+                .Select(x => new BrandViewModel { Id = x.Id, BrandName = x.BrandName})
+                .ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        [AllowAnonymous]
+        public async Task<ActionResult<BrandViewModel>> GetBrand(int id)
+        {
+            var brand = await _context.Brands.FindAsync(id);
+
+            if (brand == null)
+            {
+                return NotFound();
+            }
+
+            var brandViewModel = new BrandViewModel
+            {
+                Id = brand.Id,
+                BrandName = brand.BrandName
+            };
+
+            return brandViewModel;
+        }
+
+        [HttpPut("{id}")]
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> PutBrand(int id, BrandCreateRequest brandCreateRequest)
+        {
+            var brand = await _context.Brands.FindAsync(id);
+
+            if (brand == null)
+            {
+                return NotFound();
+            }
+
+            brand.BrandName = brandCreateRequest.BrandName;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "admin")]
+        public async Task<ActionResult<BrandViewModel>> PostBrand(BrandCreateRequest brandCreateRequest)
+        {
+            var brand = new Brand
+            {
+                BrandName = brandCreateRequest.BrandName
+            };
+
+            _context.Brands.Add(brand);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBrand", new { id = brand.Id }, new BrandViewModel { Id = brand.Id, BrandName = brand.BrandName });
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> DeleteBrand(int id)
+        {
+            var brand = await _context.Brands.FindAsync(id);
+            if (brand == null)
+            {
+                return NotFound();
+            }
+
+            _context.Brands.Remove(brand);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
