6a2466be0b248c51ce61cc32c29fe5f55199d14a
DESKTOP-D23Q2I8\shigo <vanguyen215@gmail.com>
2021-03-31 15:31:16 +0700

    Create BrandCreateRequest.cs

---
+++ RookieShopLite/ViewModel/BrandCreateRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RookieShopLite.ViewModel
+{
+    public class BrandCreateRequest
+    {
+        public string BrandName { get; set; }
+    }
+}
