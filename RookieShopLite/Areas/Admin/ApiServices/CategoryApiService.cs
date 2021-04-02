c9b83205308681ce550ea797dada0cae4af5e2db
DESKTOP-D23Q2I8\shigo <vanguyen215@gmail.com>
2021-04-02 10:49:05 +0700

    Implement CategoryApiService.cs

--- RookieShopLite/Areas/Admin/ApiServices/CategoryApiService.cs
+++ RookieShopLite/Areas/Admin/ApiServices/CategoryApiService.cs
@@ -1,9 +1,10 @@
-﻿using RookieShopLite.ViewModel;
-using System;
+﻿using Newtonsoft.Json;
+using RookieShopLite.Areas.Admin.Models;
+using RookieShopLite.ViewModel;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace RookieShopLite.Areas.Admin.ApiClients
@@ -17,12 +18,47 @@ namespace RookieShopLite.Areas.Admin.ApiClients
             _httpClientFactory = httpClientFactory;
         }
 
+        public async Task DeleteBrand(int id)
+        {
+            var httpClient = _httpClientFactory.CreateClient("local");
+            var response = await httpClient.DeleteAsync("api/categories" + $"/{id}");
+            response.EnsureSuccessStatusCode();
+        }
+
         public async Task<IList<CategoryViewModel>> GetCategories()
         {
             var httpClient = _httpClientFactory.CreateClient("local");
             var response = await httpClient.GetAsync("api/categories");
             response.EnsureSuccessStatusCode();
             return await response.Content.ReadFromJsonAsync<IList<CategoryViewModel>>();
+        }
+
+        public async Task<CategoryViewModel> GetCategory(int id)
+        {
+            var httpClient = _httpClientFactory.CreateClient("local");
+            var response = await httpClient.GetAsync("api/categories" + $"/{id}");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<CategoryViewModel>();
+        }
+
+        public async Task<CategoryViewModel> PostCategory(CategoryCreateRequest categoryCreateRequest)
+        {
+            var httpClient = _httpClientFactory.CreateClient("local");
+            var content = JsonConvert.SerializeObject(categoryCreateRequest);
+            var httpContent = new StringContent(content.ToString(), Encoding.UTF8, "application/json");
+            var response = await httpClient.PostAsync("api/categories", httpContent);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<CategoryViewModel>();
+        }
+
+        public async Task<CategoryViewModel> PutCategory(int id, CategoryCreateRequest categoryCreateRequest)
+        {
+            var httpClient = _httpClientFactory.CreateClient("local");
+            var content = JsonConvert.SerializeObject(categoryCreateRequest);
+            var httpContent = new StringContent(content.ToString(), Encoding.UTF8, "application/json");
+            var response = await httpClient.PostAsync("api/categories" + $"/{id}", httpContent);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<CategoryViewModel>();
         }
     }
 }
