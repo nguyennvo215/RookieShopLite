bac69d7252e6e5f6c5385bfd8b863050b5439af0
DESKTOP-D23Q2I8\shigo <vanguyen215@gmail.com>
2021-04-02 11:41:00 +0700

    Implement model ProductImages.cs

--- RookieShopLite/Model/ProductImages.cs
+++ RookieShopLite/Model/ProductImages.cs
@@ -7,5 +7,10 @@ namespace RookieShopLite.Model
 {
     public class ProductImages
     {
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+
+        public int ImageId { get; set; }
+        public virtual Image Image { get; set; }
     }
 }
