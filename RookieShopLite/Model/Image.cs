dd5928d08be3b8f963b2b8cedf452ff6c027cd8c
DESKTOP-D23Q2I8\shigo <vanguyen215@gmail.com>
2021-04-02 11:43:56 +0700

    Update model Image.cs

--- RookieShopLite/Model/Image.cs
+++ RookieShopLite/Model/Image.cs
@@ -1,14 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace RookieShopLite.Model
 {
     public class Image : BaseEntity
     {
-        public int ProductId { get; set; }
+        public Image()
+        {
+            ProductImages = new HashSet<ProductImages>();
+        }
         public string imgPath { get; set; }
         public DateTime AddedDate { get; set; }
         public bool isDeleted { get; set; }
-        public virtual Product Product { get; set; }
+        public virtual ICollection<ProductImages> ProductImages { get; set; }
     }
 }
