47ae81ede1ae3ec3cfc984f03a690be25ed8e2c3
DESKTOP-D23Q2I8\shigo <vanguyen215@gmail.com>
2021-04-02 11:55:37 +0700

    Create ProductImagesConfiguration

---
+++ RookieShopLite/Data/Configuration/ProductImagesConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RookieShopLite.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RookieShopLite.Data.Configuration
+{
+    public class ProductImagesConfiguration : IEntityTypeConfiguration<ProductImages>
+    {
+        public void Configure(EntityTypeBuilder<ProductImages> builder)
+        {
+            builder.HasKey(o => new { o.ProductId, o.ImageId });
+            builder
+                .HasOne(o => o.Product)
+                .WithMany(o => o.ProductImages)
+                .HasForeignKey(o => o.ImageId);
+            builder
+                .HasOne(o => o.Image)
+                .WithMany(o => o.ProductImages)
+                .HasForeignKey(o => o.ImageId);
+        }
+    }
+}
