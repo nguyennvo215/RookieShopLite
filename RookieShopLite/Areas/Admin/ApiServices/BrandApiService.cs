522841e96508622bfda3d0dcb3ce0a078ec2ce81
DESKTOP-D23Q2I8\shigo <vanguyen215@gmail.com>
2021-04-02 10:41:25 +0700

    Implement BrandApiService.cs

--- RookieShopLite/Areas/Admin/ApiServices/BrandApiService.cs
+++ RookieShopLite/Areas/Admin/ApiServices/BrandApiService.cs
@@ -1,7 +1,10 @@
-﻿using RookieShopLite.ViewModel;
+﻿using Newtonsoft.Json;
+using RookieShopLite.Areas.Admin.Models;
+using RookieShopLite.ViewModel;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Json;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace RookieShopLite.Areas.Admin.ApiClients
@@ -15,12 +18,47 @@ namespace RookieShopLite.Areas.Admin.ApiClients
             _httpClientFactory = httpClientFactory;
         }
 
+        public async Task DeleteBrand(int id)
+        {
+            var httpClient = _httpClientFactory.CreateClient("local");
+            var response = await httpClient.DeleteAsync("api/brands" + $"/{id}");
+            response.EnsureSuccessStatusCode();
+        }
+
+        public async Task<BrandViewModel> GetBrand(int id)
+        {
+            var httpClient = _httpClientFactory.CreateClient("local");
+            var response = await httpClient.GetAsync("api/brands" + $"/{id}");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<BrandViewModel>();
+        }
+
         public async Task<IList<BrandViewModel>> GetBrands()
         {
             var httpClient = _httpClientFactory.CreateClient("local");
             var response = await httpClient.GetAsync("api/brands");
             response.EnsureSuccessStatusCode();
             return await response.Content.ReadFromJsonAsync<IList<BrandViewModel>>();
+        }
+
+        public async Task<BrandViewModel> PostBrand(BrandCreateRequest brandCreateRequest)
+        {
+            var httpClient = _httpClientFactory.CreateClient("local");
+            var content = JsonConvert.SerializeObject(brandCreateRequest);
+            var httpContent = new StringContent(content.ToString(), Encoding.UTF8, "application/json");
+            var response = await httpClient.PostAsync("api/brands", httpContent);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<BrandViewModel>();
+        }
+
+        public async Task<BrandViewModel> PutBrand(int id, BrandCreateRequest brandCreateRequest)
+        {
+            var httpClient = _httpClientFactory.CreateClient("local");
+            var content = JsonConvert.SerializeObject(brandCreateRequest);
+            var httpContent = new StringContent(content.ToString(), Encoding.UTF8, "application/json");
+            var response = await httpClient.PostAsync("api/brands" + $"/{id}", httpContent);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<BrandViewModel>();
         }
     }
 }
