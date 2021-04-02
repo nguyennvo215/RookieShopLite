256424795e4aedaddb07a9571f3915150f1c770c
DESKTOP-D23Q2I8\shigo <vanguyen215@gmail.com>
2021-04-02 10:41:00 +0700

    Saturate interface IBrandApiService.cs

--- RookieShopLite/Areas/Admin/ApiServices/IBrandApiService.cs
+++ RookieShopLite/Areas/Admin/ApiServices/IBrandApiService.cs
@@ -1,4 +1,5 @@
-﻿using RookieShopLite.ViewModel;
+﻿using RookieShopLite.Areas.Admin.Models;
+using RookieShopLite.ViewModel;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -7,5 +8,9 @@ namespace RookieShopLite.Areas.Admin.ApiClients
     public interface IBrandApiService
     {
         Task<IList<BrandViewModel>> GetBrands();
+        Task<BrandViewModel> GetBrand(int id);
+        Task<BrandViewModel> PutBrand(int id, BrandCreateRequest brandCreateRequest);
+        Task<BrandViewModel> PostBrand(BrandCreateRequest brandCreateRequest);
+        Task DeleteBrand(int id);
     }
 }
