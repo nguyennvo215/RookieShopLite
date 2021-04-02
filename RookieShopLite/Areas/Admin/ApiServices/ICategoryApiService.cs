619ef684b833bbf9eb3f3cdfd85f379e4be03a0b
DESKTOP-D23Q2I8\shigo <vanguyen215@gmail.com>
2021-04-02 10:48:19 +0700

    Saturate interface ICategoryApiService.cs

--- RookieShopLite/Areas/Admin/ApiServices/ICategoryApiService.cs
+++ RookieShopLite/Areas/Admin/ApiServices/ICategoryApiService.cs
@@ -1,4 +1,5 @@
-﻿using RookieShopLite.ViewModel;
+﻿using RookieShopLite.Areas.Admin.Models;
+using RookieShopLite.ViewModel;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -7,5 +8,9 @@ namespace RookieShopLite.Areas.Admin.ApiClients
     interface ICategoryApiService
     {
         Task<IList<CategoryViewModel>> GetCategories();
+        Task<CategoryViewModel> GetCategory(int id);
+        Task<CategoryViewModel> PutCategory(int id, CategoryCreateRequest categoryCreateRequest);
+        Task<CategoryViewModel> PostCategory(CategoryCreateRequest categoryCreateRequest);
+        Task DeleteBrand(int id);
     }
 }
