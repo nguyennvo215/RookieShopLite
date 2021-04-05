0a323b9c6316b3bc82da02d391815c7d0a8b0488
DESKTOP-D23Q2I8\shigo <vanguyen215@gmail.com>
2021-04-05 10:36:17 +0700

    Update properties for ProductViewModel

--- RookieShopLite/ViewModel/ProductViewModel.cs
+++ RookieShopLite/ViewModel/ProductViewModel.cs
@@ -3,9 +3,9 @@
     public class ProductViewModel
     {
         public int Id { get; set; }
-        public string CategoryName { get; set; }
+        public double BrandId { get; set; }
+        public double CategoryId { get; set; }
         public string ProductName { get; set; }
-        public int MyProperty { get; set; }
         public string ProductShortDescription { get; set; }
         public string ProductFullDescription { get; set; }
         public double ProductPriceNow { get; set; }
