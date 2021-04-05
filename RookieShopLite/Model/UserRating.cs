926c7ea52efa374c7f3083d6d6654e2e366a1662
DESKTOP-D23Q2I8\shigo <vanguyen215@gmail.com>
2021-04-05 16:40:13 +0700

    Implement properties for UserRating and remove unnessescary usings

--- RookieShopLite/Model/UserRating.cs
+++ RookieShopLite/Model/UserRating.cs
@@ -1,11 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Identity;
 
 namespace RookieShopLite.Model
 {
     public class UserRating
     {
+        public int UserId { get; set; }
+        public int RatingId { get; set; }
+        public bool isRated { get; set; }
+        public double  RatingNumber { get; set; }
+        public virtual IdentityUser AspNetUsers { get; set; }
+        public virtual Rating Rating { get; set; }
     }
 }
