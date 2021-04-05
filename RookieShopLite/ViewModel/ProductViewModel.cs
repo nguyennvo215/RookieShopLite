651afe2e5aee124a7f0c85091816c6b85751bf67
DESKTOP-D23Q2I8\shigo <vanguyen215@gmail.com>
2021-04-05 09:33:41 +0700

    Implement ProductViewModel.cs

--- RookieShopLite/ViewModel/ProductViewModel.cs
+++ RookieShopLite/ViewModel/ProductViewModel.cs
@@ -7,5 +7,14 @@ namespace RookieShopLite.ViewModel
 {
     public class ProductViewModel
     {
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+        public string ProductName { get; set; }
+        public int MyProperty { get; set; }
+        public string ProductShortDescription { get; set; }
+        public string ProductFullDescription { get; set; }
+        public double ProductPriceNow { get; set; }
+        public double ProductPriceBefore { get; set; }
+        public bool isDeleted { get; set; }
     }
 }
