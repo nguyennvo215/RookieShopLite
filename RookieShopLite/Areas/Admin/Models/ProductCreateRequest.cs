ce6fc43dc4e3b7471caba0962a95a30bf5c565b0
DESKTOP-D23Q2I8\shigo <vanguyen215@gmail.com>
2021-04-05 10:35:49 +0700

    Implement ProductCreateRequest

---
+++ RookieShopLite/Areas/Admin/Models/ProductCreateRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RookieShopLite.Areas.Admin.Models
+{
+    public class ProductCreateRequest
+    {
+        public string ProductName { get; set; }
+        public int BrandId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductShortDescription { get; set; }
+        public string ProductFullDescription { get; set; }
+        public double ProductPriceNow { get; set; }
+        public double ProductPriceBefore { get; set; }
+    }
+}
