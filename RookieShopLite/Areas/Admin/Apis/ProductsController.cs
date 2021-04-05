fe4d89d6969c7ba05701f52be001f6e8549c002d
DESKTOP-D23Q2I8\shigo <vanguyen215@gmail.com>
2021-04-05 10:36:31 +0700

    Implement ProductsController

--- RookieShopLite/Areas/Admin/Apis/ProductsController.cs
+++ RookieShopLite/Areas/Admin/Apis/ProductsController.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RookieShopLite.Areas.Admin.Models;
+using RookieShopLite.Data;
+using RookieShopLite.Model;
+using RookieShopLite.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,81 +13,148 @@ using System.Threading.Tasks;
 
 namespace RookieShopLite.Areas.Admin.Apis
 {
-    public class ProductsController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
     {
-        // GET: ProductsController
-        public ActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
         }
 
-        // GET: ProductsController/Details/5
-        public ActionResult Details(int id)
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<ActionResult<IEnumerable<ProductViewModel>>> GetProducts()
         {
-            return View();
+            return await _context.Products
+                .Where(x => x.isDeleted == false)
+                .Select(x => new ProductViewModel 
+                { 
+                    Id = x.Id,
+                    ProductName = x.ProductName,
+                    BrandId = x.BrandId,
+                    CategoryId = x.CategoryId,
+                    ProductShortDescription = x.ProductShortDescription,
+                    ProductFullDescription = x.ProductFullDescription,
+                    ProductPriceNow = x.ProductPriceNow,
+                    ProductPriceBefore = x.ProductPriceBefore
+                })
+                .ToListAsync();
         }
 
-        // GET: ProductsController/Create
-        public ActionResult Create()
+        [HttpGet("{id}")]
+        [AllowAnonymous]
+        public async Task<ActionResult<ProductViewModel>> GetProduct(int id)
         {
-            return View();
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null || product.isDeleted == true)
+            {
+                return NotFound();
+            }
+
+            var productViewModel = new ProductViewModel
+            {
+                Id = product.Id,
+                ProductName = product.ProductName,
+                BrandId = product.BrandId,
+                CategoryId = product.CategoryId,
+                ProductShortDescription = product.ProductShortDescription,
+                ProductFullDescription = product.ProductFullDescription,
+                ProductPriceNow = product.ProductPriceNow,
+                ProductPriceBefore = product.ProductPriceBefore
+            };
+
+            return productViewModel;
         }
 
-        // POST: ProductsController/Create
+        [HttpPut("{id}")]
+        [AllowAnonymous]
+        public async Task<ActionResult<ProductViewModel>> PutProduct(int id, ProductCreateRequest productCreateRequest)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null || product.isDeleted == true)
+            {
+                return NotFound();
+            }
+
+            product.ProductName = productCreateRequest.ProductName;
+            product.ProductShortDescription = productCreateRequest.ProductShortDescription;
+            product.ProductFullDescription = productCreateRequest.ProductFullDescription;
+            product.ProductPriceNow = productCreateRequest.ProductPriceNow;
+            product.CategoryId = productCreateRequest.CategoryId;
+            product.BrandId = productCreateRequest.BrandId;
+            product.AddedDate = DateTime.Now;
+            product.AddedBy = HttpContext.User.Identity.Name;
+            await _context.SaveChangesAsync();
+
+            var productViewModel = new ProductViewModel
+            {
+                Id = product.Id,
+                ProductName = product.ProductName,
+                ProductShortDescription = product.ProductShortDescription,
+                ProductFullDescription = product.ProductFullDescription,
+                ProductPriceNow = product.ProductPriceNow,
+                ProductPriceBefore = productCreateRequest.ProductPriceBefore,
+                CategoryId = product.CategoryId,
+                BrandId = product.BrandId
+            };
+
+            return productViewModel;
+        }
+
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        [AllowAnonymous]
+        public async Task<ActionResult<ProductViewModel>> PostProduct(ProductCreateRequest productCreateRequest)
         {
-            try
+            var product = new Product
             {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+                ProductName = productCreateRequest.ProductName,
+                ProductShortDescription = productCreateRequest.ProductShortDescription,
+                ProductFullDescription = productCreateRequest.ProductFullDescription,
+                ProductPriceNow = productCreateRequest.ProductPriceNow,
+                CategoryId = productCreateRequest.CategoryId,
+                BrandId = productCreateRequest.BrandId,
+                AddedDate = DateTime.Now,
+                AddedBy = HttpContext.User.Identity.Name
+                
+            };
+
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBrand", new { id = product.Id },
+                new ProductViewModel 
+                {
+                    Id = product.Id,
+                    ProductName = product.ProductName,
+                    ProductShortDescription = product.ProductShortDescription,
+                    ProductFullDescription = product.ProductFullDescription,
+                    ProductPriceNow = product.ProductPriceNow,
+                    ProductPriceBefore = productCreateRequest.ProductPriceBefore,
+                    CategoryId = product.CategoryId,
+                    BrandId = product.BrandId
+                });
         }
 
-        // GET: ProductsController/Edit/5
-        public ActionResult Edit(int id)
+        [HttpDelete("{id}")]
+        [AllowAnonymous]
+        public async Task<IActionResult> DeleteProduct(int id)
         {
-            return View();
-        }
+            var product = await _context.Products.FindAsync(id);
+            if (product == null || product.isDeleted == true)
+            {
+                return NotFound();
+            }
 
-        // POST: ProductsController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+            product.isDeleted = true;
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
 
-        // GET: ProductsController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: ProductsController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            return NoContent();
         }
     }
 }
