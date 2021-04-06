d5cd44b15e7647f46b8f1ccd30acdeb257923a28
DESKTOP-D23Q2I8\shigo <vanguyen215@gmail.com>
2021-04-06 10:07:18 +0700

    Remove apply config for productimages in dbcontext

--- RookieShopLite/Data/ApplicationDbContext.cs
+++ RookieShopLite/Data/ApplicationDbContext.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using RookieShopLite.Data.Configuration;
 using RookieShopLite.Model;
 
 namespace RookieShopLite.Data
@@ -17,13 +16,6 @@ namespace RookieShopLite.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<Image> Images { get; set; }
         public DbSet<Product> Products { get; set; }
-        public DbSet<ProductImages> ProductImages { get; set; }
         public DbSet<RetailDetail> RetailDetails { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.ApplyConfiguration(new ProductImagesConfiguration());
-            base.OnModelCreating(modelBuilder);
-        }
     }
 }
