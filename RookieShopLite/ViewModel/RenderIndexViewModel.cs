9b999736ddf5c58d852d2b51e910d7f762cf6269
DESKTOP-D23Q2I8\shigo <vanguyen215@gmail.com>
2021-04-08 11:28:30 +0700

    Add new ViewModel to render the index page

---
+++ RookieShopLite/ViewModel/RenderIndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿using RookieShopLite.Model;
+using System.Collections.Generic;
+
+namespace RookieShopLite.ViewModel
+{
+    public class RenderIndexViewModel
+    {
+        public List<Product> Products { get; set; }
+        public string SearchString { get; set; }
+    }
+}
