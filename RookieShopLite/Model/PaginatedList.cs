e277b01eba7674dabaa3c602c943aa14ce751382
DESKTOP-D23Q2I8\shigo <vanguyen215@gmail.com>
2021-04-08 14:04:00 +0700

    Add PaginatedList.cs to model folder

--- RookieShopLite/Model/PaginatedList.cs
+++ RookieShopLite/Model/PaginatedList.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 
-namespace RookieShopLite
+namespace RookieShopLite.Model
 {
     public class PaginatedList<T> : List<T>
     {
