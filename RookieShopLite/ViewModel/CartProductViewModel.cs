37348c3df8d2df06acddbe155fd2682965b1278c
DESKTOP-D23Q2I8\shigo <vanguyen215@gmail.com>
2021-04-14 14:22:30 +0700

    Saturate CartProduct view model

---
+++ RookieShopLite/ViewModel/CartProductViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RookieShopLite.ViewModel
+{
+    public class CartProductViewModel
+    {
+        public int CartId { get; set; }
+        public string ProductName { get; set; }
+        public decimal ProductPriceBefore { get; set; }
+        public decimal ProductPriceAfter { get; set; }
+        public string imgPath { get; set; }
+    }
+}
