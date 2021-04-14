005cddb4c3dc029172d7bd1d63b28aaf562ab56e
DESKTOP-D23Q2I8\shigo <vanguyen215@gmail.com>
2021-04-14 14:53:21 +0700

    Create empty CartApiService and interface in apis folder

---
+++ RookieShopLite/Areas/Admin/ApiServices/Cart/ICartApiService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RookieShopLite.Areas.Admin.ApiServices.Cart
+{
+    public class ICartApiService
+    {
+    }
+}
