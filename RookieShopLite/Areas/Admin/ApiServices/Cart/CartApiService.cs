32ef4bd35717d6ac7b51a5d18c531c04e791bf91
DESKTOP-D23Q2I8\shigo <vanguyen215@gmail.com>
2021-04-16 09:34:19 +0700

    implement cart api services

--- RookieShopLite/Areas/Admin/ApiServices/Cart/CartApiService.cs
+++ RookieShopLite/Areas/Admin/ApiServices/Cart/CartApiService.cs
@@ -1,11 +1,47 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using RookieShopLite.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace RookieShopLite.Areas.Admin.ApiServices.Cart
 {
-    public class CartApiService
+    public class CartApiService : ICartApiService
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CartApiService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task AddToCart(int id)
+        {
+            var httpClient = _httpClientFactory.CreateClient("local");
+            var content = JsonConvert.SerializeObject(id);
+            var httpContent = new StringContent(content.ToString(), Encoding.UTF8, "application/json");
+            var response = await httpClient.PostAsync("api/carts", httpContent);
+            response.EnsureSuccessStatusCode();
+        }
+
+        public async Task<IList<CartViewModel>> GetCartHistory()
+        {
+            var httpClient = _httpClientFactory.CreateClient("local");
+            var response = await httpClient.GetAsync("api/carts");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<IList<CartViewModel>>();
+        }
+
+        public async Task<CartViewModel> GetCurrentCart()
+        {
+            var httpClient = _httpClientFactory.CreateClient("local");
+            var response = await httpClient.GetAsync("api/carts");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<CartViewModel>();
+        }
     }
 }
