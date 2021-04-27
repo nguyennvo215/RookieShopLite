fd55460694848fd383fb3df5f3706c29d8ca9f0d
shigoyabuki <shigo3598@gmail.com>
2021-04-27 22:27:05 +0700

    Add user controller to get all users

---
+++ RookieShopLite/Areas/Admin/Apis/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RookieShopLite.Data;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RookieShopLite.Areas.Admin.Apis
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<ActionResult<IEnumerable<IdentityUser>>> GetCustomers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+    }
+}
