555568e4da0996e3602b7c4859c59fab28c9d566
shigoyabuki <shigo3598@gmail.com>
2021-04-28 02:06:14 +0700

    Create Category List ViewComponent

---
+++ RookieShopLite/ViewComponents/CategoryListViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RookieShopLite.Areas.Admin.ApiServices.Category;
+using System.Threading.Tasks;
+
+namespace RookieShopLite.ViewComponents
+{
+    public class CategoryListViewComponent : ViewComponent
+    {
+        private readonly ICategoryApiService _category;
+
+        public CategoryListViewComponent(ICategoryApiService category)
+        {
+            _category = category;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var items = await _category.GetCategories();
+            return View(items);
+        }
+    }
+}
