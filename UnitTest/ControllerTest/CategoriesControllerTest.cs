4ebdcf0217003737c634e7c0672ac20860d768a8
DESKTOP-D23Q2I8\shigo <vanguyen215@gmail.com>
2021-04-29 10:34:46 +0700

    Create test for update on category controller

--- UnitTest/ControllerTest/CategoriesControllerTest.cs
+++ UnitTest/ControllerTest/CategoriesControllerTest.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using RookieShopLite.Areas.Admin.Apis;
 using RookieShopLite.Areas.Admin.Models;
+using RookieShopLite.Model;
 using RookieShopLite.ViewModel;
+using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -29,6 +32,23 @@ namespace UnitTest.ControllerTest
             var createdAtActionResult = Assert.IsType<CreatedAtActionResult>(result.Result);
             var returnValue = Assert.IsType<CategoryViewModel>(createdAtActionResult.Value);
             Assert.Equal("Test category", returnValue.CategoryName);
+        }
+
+        [Fact]
+        public async Task PutCategory_Success()
+        {
+            var dbContext = _fixture.Context;
+            dbContext.Categories.Add(new Category { CategoryName = "Test category" });
+            await dbContext.SaveChangesAsync();
+
+            var oldCategory = await dbContext.Categories.OrderByDescending(x => x.Id).FirstAsync();
+            var category = new CategoryCreateRequest { CategoryName = "Test put category" };
+
+            var controller = new CategoriesController(dbContext);
+            var result = await controller.PutCategory(oldCategory.Id, category);
+
+            var returnValue = await dbContext.Categories.OrderByDescending(x => x.Id).FirstAsync();
+            Assert.Equal("Test put category", returnValue.CategoryName);
         }
     }
 }
