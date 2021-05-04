626bda9c70985ed8d37dc1210c9b608870f8e740
DESKTOP-D23Q2I8\shigo <vanguyen215@gmail.com>
2021-05-04 18:14:34 +0700

    Update seed data

--- RookieShopLite/Data/DbInitializer.cs
+++ RookieShopLite/Data/DbInitializer.cs
@@ -34,8 +34,8 @@ namespace RookieShopLite.Data
 
             var products = new Product[]
             {
-                new Product{ ProductName = "IPhone", BrandId = 2, CategoryId = 1, ProductShortDescription = "Iphone", isDeleted = false, ProductPriceNow = 200},
-                new Product{ ProductName = "Samsung", BrandId = 1, CategoryId = 1, ProductShortDescription = "Samsung", isDeleted = false, ProductPriceNow = 250, ProductPriceBefore = 350}
+                new Product{ ProductName = "IPhone", BrandId = 2, CategoryId = 1, ProductShortDescription = "Iphone", ProductFullDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec pellentesque risus. Morbi efficitur ultricies augue sed porttitor. Vestibulum accumsan dignissim ex, at tempus nibh faucibus sed. Fusce venenatis porta mi quis rhoncus. Integer vestibulum congue nulla, maximus sollicitudin mi congue id. Suspendisse et urna nulla. Donec sed tortor in quam convallis placerat. Praesent placerat urna vel augue pretium egestas. Praesent nibh purus, maximus a dui eget, rutrum interdum lacus.", isDeleted = false, ProductPriceNow = 200},
+                new Product{ ProductName = "Samsung", BrandId = 1, CategoryId = 1, ProductShortDescription = "Samsung", ProductFullDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec pellentesque risus. Morbi efficitur ultricies augue sed porttitor. Vestibulum accumsan dignissim ex, at tempus nibh faucibus sed. Fusce venenatis porta mi quis rhoncus. Integer vestibulum congue nulla, maximus sollicitudin mi congue id. Suspendisse et urna nulla. Donec sed tortor in quam convallis placerat. Praesent placerat urna vel augue pretium egestas. Praesent nibh purus, maximus a dui eget, rutrum interdum lacus.", isDeleted = false, ProductPriceNow = 250, ProductPriceBefore = 350}
             };
 
             context.Products.AddRange(products);
@@ -43,9 +43,9 @@ namespace RookieShopLite.Data
 
             var images = new ProductImage[] 
             {
-                new ProductImage { ProductId = 1, imgPath = "products/1.jpg", isDeleted = false },
-                new ProductImage { ProductId = 2, imgPath = "products/2.jpg", isDeleted = false },
-                new ProductImage { ProductId = 1, imgPath = "products/3.jpg", isDeleted = false }
+                new ProductImage { ProductId = 1, imgPath = "https://res.cloudinary.com/dfzg0xvjj/image/upload/v1620126369/eh44lrm9yds8aa8opgzz.jpg", isDeleted = false },
+                new ProductImage { ProductId = 2, imgPath = "https://res.cloudinary.com/dfzg0xvjj/image/upload/v1620126419/yhsawbzapn897nfmlfyi.jpg", isDeleted = false },
+                new ProductImage { ProductId = 1, imgPath = "https://res.cloudinary.com/dfzg0xvjj/image/upload/v1620126526/fohql1utma2ffvmx4mbz.jpg", isDeleted = false }
             };
 
             context.Images.AddRange(images);
